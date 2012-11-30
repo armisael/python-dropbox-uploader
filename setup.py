41f10b682d03faea1c289733fb089666ff42d2c4
Stefano Parmesan <stefanop@ahref.eu>
2012-11-30 20:41:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-dropbox-uploader',
+    version='0.0.1',
+    author='armisael',
+    author_email='armisael@silix.org',
+    url='http://www.github.com/armisael/python-dropbox-uploader',
+    description='Easily upload files on your dropbox account',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
